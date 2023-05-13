1754fb4cba7619415bbf129258c722a89452a62a
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-05-13 20:05:36 +0300

    Added annotations for creating a new shops table

--- src/main/java/by/tms/entity/shop/Shop.java
+++ src/main/java/by/tms/entity/shop/Shop.java
@@ -1,19 +1,19 @@
 package by.tms.entity.shop;
 
-import by.tms.entity.Category;
-import by.tms.entity.Product;
-import by.tms.entity.Section;
 import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "shops")
 @Setter @Getter
 @Builder @NoArgsConstructor @AllArgsConstructor
 public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
     private String phone;
-    private String address;
-    private Category category;
-    private Section section;
-    private Product product;
 }
