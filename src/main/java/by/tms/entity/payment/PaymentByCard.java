d7502e6b4e3372ac5feaac074c41a2cac828b0d4
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-05-13 20:54:27 +0300

    Added annotations for creating new payment tables

--- src/main/java/by/tms/entity/payment/PaymentByCard.java
+++ src/main/java/by/tms/entity/payment/PaymentByCard.java
@@ -2,9 +2,17 @@ package by.tms.entity.payment;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Setter @Getter
 @Builder @NoArgsConstructor @AllArgsConstructor
 public class PaymentByCard implements PaymentType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String cardNumber;
 }
