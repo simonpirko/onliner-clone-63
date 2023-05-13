796c2e13f7768d56ba8c6e04c57423b1746e444c
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-05-13 20:46:26 +0300

    Added annotations for creating new parameter tables

--- src/main/java/by/tms/entity/parameter/ParameterFactory.java
+++ src/main/java/by/tms/entity/parameter/ParameterFactory.java
@@ -1,16 +1,30 @@
 package by.tms.entity.parameter;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.Optional;
 
+@Entity
+@Setter @Getter
+@Builder @NoArgsConstructor @AllArgsConstructor
 public class ParameterFactory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne
+    PhoneParameter phoneParameter = null;
+    @OneToOne
+    LaptopParameter laptopParameter = null;
+
     public Optional<Parameter> add(Parameter.Type type) {
         switch (type) {
             case PHONE: {
-                PhoneParameter phoneParameter = new PhoneParameter();
+                phoneParameter = new PhoneParameter();
                 return Optional.of(phoneParameter);
             }
             case LAPTOP: {
-                LaptopParameter laptopParameter = new LaptopParameter();
+                laptopParameter = new LaptopParameter();
                 return Optional.of(laptopParameter);
             }
         }
