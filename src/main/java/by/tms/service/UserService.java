3e25312fba0bea2caa20049e1eea0cab33bac0c4
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-05-25 14:29:18 +0300

    Added a class UserService

---
+++ src/main/java/by/tms/service/UserService.java
@@ -0,0 +1,33 @@
+package by.tms.service;
+
+import by.tms.dao.HibernateUserDao;
+import by.tms.dao.UserDao;
+import by.tms.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserDao userDao = new HibernateUserDao();
+
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    public void remove(long id) {
+        userDao.remove(id);
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    public User findById(long id) {
+        return userDao.findById(id);
+    }
+
+    public User findByProductName(String name) {
+        return userDao.findByUsername(name);
+    }
+}
