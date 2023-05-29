c9cf2897d1e40be52edab6c3e35bffed2b4c3778
Arina <arina.milyutina.0005@gmail.com>
2023-05-29 21:58:45 +0300

    add methods save and findByUsername

--- src/main/java/by/tms/dao/HibernateUserDao.java
+++ src/main/java/by/tms/dao/HibernateUserDao.java
@@ -1,5 +1,33 @@
 package by.tms.dao;
 
+import by.tms.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
 public class HibernateUserDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+    private static final String FIND_BY_USERNAME = "from User where username = :username";
+    private static final String USERNAME = "username";
+
+
+    @Transactional
+    public void save(User user) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Query<User> query = currentSession.createQuery(FIND_BY_USERNAME);
+        query.setParameter(USERNAME, username);
+        User singleResult = query.getSingleResult();
+        return Optional.of(singleResult);
+    }
 
 }
