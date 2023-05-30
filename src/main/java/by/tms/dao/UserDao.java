78daf9fab3d4755fcd8a6faf6a4aa43c1a315534
Arina <arina.milyutina.0005@gmail.com>
2023-05-30 20:45:09 +0300

    delete

--- src/main/java/by/tms/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package by.tms.dao;
-
-import by.tms.entity.User;
-import org.springframework.transaction.annotation.Transactional;
-
-public interface UserDao {
-    @Transactional
-    void save(User user);
-}
