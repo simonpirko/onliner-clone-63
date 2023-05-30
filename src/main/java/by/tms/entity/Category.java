22cc1b935d426f79fac3d92767516afd1387a3b9
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-05-30 14:34:17 +0300

    Added enums Parameter and Category

--- src/main/java/by/tms/entity/Category.java
+++ src/main/java/by/tms/entity/Category.java
@@ -1,12 +1,5 @@
 package by.tms.entity;
-import lombok.*;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "categories")
-@Setter @Getter
-@Builder @NoArgsConstructor @AllArgsConstructor
-public class Category extends AbstractEntity{
-    private String name;
+public enum Category{
+    LAPTOP, PHONES, HEADPHONES, TV
 }
--- src/main/java/by/tms/entity/parameter/Parameter.java
+++ src/main/java/by/tms/entity/parameter/Parameter.java
@@ -1,15 +1,8 @@
 package by.tms.entity.parameter;
 
-import by.tms.entity.AbstractEntity;
-import lombok.*;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "parameters")
-@Setter @Getter
-@Builder @NoArgsConstructor @AllArgsConstructor
-public class Parameter extends AbstractEntity {
-    private String name;
+public enum Parameter{
+    SCREEN_SIZE,
+    BATTERY_CAPACITY,
+    COLOR,
+    CAMERA
 }
