26c3298511fa7862231762ad344446d84976a264
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-04 23:56:07 +0300

    Added validation

--- src/main/java/by/tms/dto/CreateProductDto.java
+++ src/main/java/by/tms/dto/CreateProductDto.java
@@ -6,13 +6,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor @AllArgsConstructor
 public class CreateProductDto {
+    @NotBlank(message = "not blank")
+    @NotEmpty(message = "not empty")
     private String name;
+    @NotBlank(message = "not blank")
+    @NotEmpty(message = "not empty")
     private String itemNumber;
+    @Min(value = 0)
     private long quantity;
+    @NotBlank(message = "not blank")
+    @NotEmpty(message = "not empty")
     private String image;
+    @Min(value = 0)
     private double price;
     private Category category;
     private Parameter parameter1;
