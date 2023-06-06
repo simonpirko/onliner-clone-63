5fc2d489bd6e5f94c138b45dfa2d2b9948be9371
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-06 19:08:31 +0300

    Created packages for dao

--- src/main/java/by/tms/service/ProductParameterService.java
+++ src/main/java/by/tms/service/ProductParameterService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 public class ProductParameterService {
     @Autowired
     private ProductParameterDao productParameterDao;
