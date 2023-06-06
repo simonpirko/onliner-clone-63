fa12572b27d64844febd557ce6e7a7515b72c420
Arina <arina.milyutina.0005@gmail.com>
2023-06-06 13:56:19 +0300

    create AuthorizationUserDto

---
+++ src/main/java/by/tms/dto/AuthorizationUserDto.java
@@ -0,0 +1,14 @@
+package by.tms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorizationUserDto {
+    private String name;
+    private String username;
+    private String password;
+}
