29ffb6f088ab4654f6fe2a3d9701ae27ddb909bc
Arina <arina.milyutina.0005@gmail.com>
2023-06-06 13:54:33 +0300

    add annotation Transactional

--- src/main/java/by/tms/service/UserService.java
+++ src/main/java/by/tms/service/UserService.java
@@ -4,10 +4,12 @@ import by.tms.dao.HibernateUserDao;
 import by.tms.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserService {
     @Autowired
     private HibernateUserDao hibernateUserDao;
