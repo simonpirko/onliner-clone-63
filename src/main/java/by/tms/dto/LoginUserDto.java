4c771b31bbd880f00dae5f75ff8fde6efaa44f6a
Arina <arina.milyutina.0005@gmail.com>
2023-06-06 14:24:39 +0300

    delete field name

--- src/main/java/by/tms/dto/LoginUserDto.java
+++ src/main/java/by/tms/dto/LoginUserDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginUserDto {
-    private String name;
     private String username;
     private String password;
 }
